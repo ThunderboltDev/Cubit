a23e192494f60c1efb84c5e0ccb5ee3fc171bc60
Programmer80101 <programmer80101@gmail.com>
2026-02-22 18:57:45 +0530

    fix scramble generation

--- src/contexts/scramble.tsx
+++ src/contexts/scramble.tsx
@@ -11,13 +11,6 @@ import {
 import { usePuzzles } from "@/hooks/use-puzzles";
 import { generateScramble } from "@/lib/scrambles";
 
-function useMount(fn: () => void) {
-  // biome-ignore lint/correctness/useExhaustiveDependencies: hook
-  useEffect(() => {
-    fn();
-  }, []);
-}
-
 interface ScrambleContextType {
   scramble: string;
   scrambleRef: RefObject<string>;
@@ -28,51 +21,27 @@ const ScrambleContext = createContext<ScrambleContextType | null>(null);
 
 export function ScrambleProvider({ children }: { children: ReactNode }) {
   const { currentPuzzle } = usePuzzles();
-
   const [scramble, setScramble] = useState<string>("Generating scramble...");
-
-  const isMounted = useRef(false);
   const scrambleRef = useRef(scramble);
 
-  useMount(() => {
-    isMounted.current = true;
-
-    if (currentPuzzle) {
-      generateScramble(currentPuzzle.type).then((newScramble) => {
-        if (isMounted.current) {
-          setScramble(newScramble);
-          scrambleRef.current = newScramble;
-        }
-      });
-    }
-  });
-
-  // useEffect(() => {
-  //   scrambleRef.current = scramble;
-  // }, [scramble]);
-
   useEffect(() => {
-    if (currentPuzzle) {
-      generateScramble(currentPuzzle.type).then((newScramble) => {
-        setScramble(newScramble);
-        scrambleRef.current = newScramble;
-      });
-    }
+    if (!currentPuzzle) return;
+    generateScramble(currentPuzzle.type).then((newScramble) => {
+      setScramble(newScramble);
+      scrambleRef.current = newScramble;
+    });
   }, [currentPuzzle]);
 
   const generateNewScramble = useCallback(() => {
-    if (currentPuzzle) {
-      generateScramble(currentPuzzle.type).then((newScramble) => {
-        setScramble(newScramble);
-        scrambleRef.current = newScramble;
-      });
-    }
+    if (!currentPuzzle) return;
+    generateScramble(currentPuzzle.type).then((newScramble) => {
+      setScramble(newScramble);
+      scrambleRef.current = newScramble;
+    });
   }, [currentPuzzle]);
 
   return (
-    <ScrambleContext.Provider
-      value={{ scramble, scrambleRef, generateNewScramble }}
-    >
+    <ScrambleContext.Provider value={{ scramble, scrambleRef, generateNewScramble }}>
       {children}
     </ScrambleContext.Provider>
   );
@@ -80,10 +49,6 @@ export function ScrambleProvider({ children }: { children: ReactNode }) {
 
 export function useScramble() {
   const context = useContext(ScrambleContext);
-
-  if (!context) {
-    throw new Error("useScramble must be used within ScrambleProvider");
-  }
-
+  if (!context) throw new Error("useScramble must be used within ScrambleProvider");
   return context;
 }
